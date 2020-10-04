1b0273cd8d160d8bd8141244162b1bcfd74dcf56
Kirsh <396989406@qq.com>
2020-10-04 17:05:19 +0800

    Initial commit

---
+++ utils.js
@@ -0,0 +1,23 @@
+var Utils = function () {
+    //私有属性
+    var username;
+
+    //私有方法
+    var setUsername = function (username) {
+        this.username = username;
+    };
+
+    var getUsername = function () {
+        return this.username;
+    };
+
+    return {
+        //公共方法
+        setUsername: function (username) {
+            setUsername(username);
+        },
+        getUsername: function () {
+            return getUsername();
+        }
+    }
+}();
